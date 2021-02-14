24cae685283ef4360e455eca984daed0f386b4bd
Alejandro Manuel Vigil Quintanilla <alejandrovigil150@icloud.com>
2021-02-14 12:17:20 -0600

    creacion y publicacion de la rama controles avanzados

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -53,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
         tbhConversores.addTab(tbhConversores.newTabSpec("Monedas").setContent(R.id.tabMonedas));
         tbhConversores.addTab(tbhConversores.newTabSpec("Longitud").setContent(R.id.tabLongitud));
-        tbhConversores.addTab(tbhConversores.newTabSpec("Masa").setContent(R.id.tabMasa));
-
+        tbhConversores.addTab(tbhConversores.newTabSpec("Tiempo").setContent(R.id.tabTiempo));
+        tbhConversores.addTab(tbhConversores.newTabSpec("Volumen").setContent(R.id.tabVolumen));
         btnConvertir = findViewById(R.id.btnCalcular);
         btnConvertir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,11 +99,13 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
+
 class conversores{
     double[][] conversor = {
             {1.0,8.75,7.77, 24.03,34.8,611.10,0.82,105,3545.93,20.05},/*Monedas*/
             {1.0, 100.0,39.37,3.28,1.09,0.000621,0.00054,1000,1000000,1000000000,1.19},/*Longitud*/
-            {1.0}/*Masa*/
+            {1.00,8.64e+7,86400.00,1440.00,24.00,0.142857,0.0328767,0.00273973,0.000273973,2.7397e-5},/*Tiempo*/
+            {1.00,1000.00,1000.00,0.001,202.88,67.628,4.22,1.056,0.26,61.023},/*Volumen*/
     };
     public double convertir(int opcion, int de, int a, double cantidad){
         return conversor[opcion][a] / conversor[opcion][de] * cantidad;

37ef3e54bc146ed792ac0ebc9e515e98e8718f8a
Alejandro Manuel Vigil Quintanilla <alejandrovigil150@icloud.com>
2021-03-05 11:28:28 -0600

    Parcial_1

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -33,76 +33,13 @@ import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-    TabHost tbhConversores;
-    Button btnConvertir;
-    TextView tempVal;
-    Spinner spnOpcionDe, spnOpcionA;
-    conversores miConversor = new conversores();
-    RelativeLayout contenidoView;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        tbhConversores = findViewById(R.id.tbhConversores);
-        tbhConversores.setup();
-
-        tbhConversores.addTab(tbhConversores.newTabSpec("Superficie").setContent(R.id.tabSuperficie));
-
-        btnConvertir = findViewById(R.id.btnCalcular);
-        btnConvertir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    tempVal = (TextView) findViewById(R.id.txtcantidad);
-                    double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                    spnOpcionDe = findViewById(R.id.cboDe);
-                    spnOpcionA = findViewById(R.id.cboA);
-                    tempVal = findViewById(R.id.lblRespuesta);
-                    tempVal.setText("Respuesta: " + miConversor.convertir(0, spnOpcionDe.getSelectedItemPosition(), spnOpcionA.getSelectedItemPosition(), cantidad));
-                }catch (Exception e){
-                    tempVal = findViewById(R.id.lblRespuesta);
-                    tempVal.setText("Por favor ingrese los valores correspondiente");
-                    Toast.makeText(getApplicationContext(), "Por ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
-
-                }
-            }
-        });
-        btnConvertir = findViewById(R.id.btnCalcularL);
-        btnConvertir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    tempVal = findViewById(R.id.txtCantidadL);
-                    double cantidad = Double.parseDouble(tempVal.getText().toString());
-
-                    spnOpcionDe = findViewById(R.id.cboDeL);
-                    spnOpcionA = findViewById(R.id.cboAL);
-                    tempVal = findViewById(R.id.lblRespuestaL);
-
-                    tempVal.setText("Respuesta: " + miConversor.convertir(1, spnOpcionDe.getSelectedItemPosition(), spnOpcionA.getSelectedItemPosition(), cantidad));
-                }catch (Exception e){
-                    tempVal = findViewById(R.id.lblRespuestaL);
-                    tempVal.setText("Por favor ingrese los valores correspondiente");
-                    Toast.makeText(getApplicationContext(), "Por ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
     }
 }
 
 
-class conversores{
-    double[][] conversor = {
-            {1.0,8.75,7.77, 24.03,34.8,611.10,0.82,105,3545.93,20.05},/*Monedas*/
-
-    public double convertir(int opcion, int de, int a, double cantidad){
-        return conversor[opcion][a] / conversor[opcion][de] * cantidad;
-    }
-}

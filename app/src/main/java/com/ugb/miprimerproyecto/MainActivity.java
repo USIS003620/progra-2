6d197467d15df5cea37cd03a0cf6531a66e83f45
Alejandro Manuel Vigil Quintanilla <alejandrovigil150@icloud.com>
2021-03-06 20:27:23 -0600

    Parcial_1

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -2,44 +2,67 @@ package com.ugb.miprimerproyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TabHost;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.BaseTransientBottomBar;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    TabHost tbhConversores;
+    Button btnConvertir;
+    TextView tempVal;
+    Spinner spnOpcionDe, spnOpcionA;
+    conversores miConversor = new conversores();
+    RelativeLayout contenidoView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        tbhConversores.addTab(tbhConversores.newTabSpec("Superficie").setContent(R.id.tabSuperficie));
+
+        btnConvertir = findViewById(R.id.btnCalcular);
+        btnConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    tempVal = (TextView) findViewById(R.id.txtcantidad);
+                    double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+                    spnOpcionDe = findViewById(R.id.cboDe);
+                    spnOpcionA = findViewById(R.id.cboA);
+                    tempVal = findViewById(R.id.lblRespuesta);
+                    tempVal.setText("Respuesta: " + miConversor.convertir(0, spnOpcionDe.getSelectedItemPosition(), spnOpcionA.getSelectedItemPosition(), cantidad));
+                }catch (Exception e){
+                    tempVal = findViewById(R.id.lblRespuesta);
+                    tempVal.setText("Por favor ingrese los valores correspondiente");
+                    Toast.makeText(getApplicationContext(), "Por favor ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
 }
 
+
+class conversores{
+    double[][] conversor = {
+            {0.092903, 0.698896, 0.836127,1, 628.8, 7000, 10000},/*Superficie*/
+
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return conversor[opcion][a] / conversor[opcion][de] * cantidad;
+    }
+}
 

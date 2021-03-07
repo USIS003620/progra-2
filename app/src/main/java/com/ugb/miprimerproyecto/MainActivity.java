19bd1482bef821bd872118785781bd0b9f72bea1
Wilbert Ruth <octavioeaevab@gmail.com>
2021-03-07 15:30:42 -0800

    Se agrego iconos y tabhost del sitema de agua.

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -1,16 +1,18 @@
 package com.ugb.miprimerproyecto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
     TabHost tbhConversores;
@@ -20,16 +22,16 @@ public class MainActivity extends AppCompatActivity {
     conversores miConversor = new conversores();
     RelativeLayout contenidoView;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        tbhConversores.addTab(tbhConversores.newTabSpec("Superficie").setContent(R.id.tabSuperficie));
+        contenidoView = findViewById(R.id.contenidoView);
+        tbhConversores = findViewById(R.id.tbhconversores);
+        tbhConversores.setup();
+        tbhConversores.addTab(tbhConversores.newTabSpec("Superficie").setContent(R.id.tabsuperficie).setIndicator("",getResources().getDrawable(R.drawable.ic_money)));
+        tbhConversores.addTab(tbhConversores.newTabSpec("Superficie").setContent(R.id.tab2).setIndicator("",getResources().getDrawable(R.drawable.ic_iconfinder_hygiene_23_4443488)));
 
         btnConvertir = findViewById(R.id.btnCalcular);
         btnConvertir.setOnClickListener(new View.OnClickListener() {
@@ -46,10 +48,34 @@ public class MainActivity extends AppCompatActivity {
                 }catch (Exception e){
                     tempVal = findViewById(R.id.lblRespuesta);
                     tempVal.setText("Por favor ingrese los valores correspondiente");
-                    Toast.makeText(getApplicationContext(), "Por favor ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(getApplicationContext(), "Por ingrese los valores correspondiente "+ e.getMessage(),Toast.LENGTH_SHORT).show();
                 }
             }
+        });
+        btnConvertir = findViewById(R.id.btnCalcular1);
+        btnConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView respuesta = findViewById(R.id.lblRespuesta1);
+                EditText num = findViewById(R.id.txtcantidad1);
+                double valor = Double.parseDouble(num.getText().toString());
+
+                double respuest = 6;
+                if (valor >= 18){
+                 respuest= 6;
+
+                }
+               if (valor >= 19){
+               respuest = valor - 16 * 0.45 + 6 - 14.8;
+
+               }
+               if (valor >= 29 ){
+               respuest = valor -26 * 0.65 - 18 * 0.45 + 6.5 + 4.5 + 6 - 13;
+
+               }
+                respuesta.setText(String.valueOf(respuest));
+            }
+
         });
 
     }

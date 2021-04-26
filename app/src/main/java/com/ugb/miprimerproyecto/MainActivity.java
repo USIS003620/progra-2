5ce4fe4f15075576969a6a0e6682efc6a566d162
Wilbert Ruth <octavioeaevab@gmail.com>
2021-04-25 20:36:41 -0700

    Se agrego coneccion a la base de datos y codigo fuente...

--- app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
+++ app/src/main/java/com/ugb/miprimerproyecto/MainActivity.java
@@ -3,7 +3,6 @@ package com.ugb.miprimerproyecto;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
